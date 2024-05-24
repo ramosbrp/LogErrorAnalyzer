a850df96fa17dcb2a12aa085ba2acf2c5bd1d74a
Bruno <bruno.rp45@gmail.com>
2024-05-24 14:05:59 -0300

    Block.cs; LogProcesso; .gitignore

---
+++ LogErrorAnalyzer/Block.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogErrorAnalyzer
+{
+    internal class Block
+    {
+        public List<string> Lines { get; set; } = new List<string>();
+        public bool HasError { get; set; } = false;
+        public string MensagemHash { get; set; }
+        public string RealmInicial { get; set; }
+    }
+}
